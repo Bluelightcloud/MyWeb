bb25abff0ac9dfbaac613106e6b052084b23cb16
blcloud <nekurina@gmail.com>
2021-07-27 17:14:32 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8" />
+        <title>Bluelightcloud's HomePage</title>
+    </head>
+    
+    <body bgcolor="	#EEFFFF">
+    	<br>
+    	<h1><center><font color=#000000>Bluelightcloud's HomePage</font></center></h1>
+    	<hr>
+    	<div class="Profile" style="margin:7%;">
+    		<h2>◇自己紹介</h2>
+    		<p>こんにちは。くもです。OPIとも呼ばれてます。</p>
+    		<p>バックエンド寄りのコーディングをしつつ、傍らにゲームのサーバ構築運用をしています。</p>
+    		<p>近況 : メインPCを買い替えたいのに高くて困ってます。</p>
+    		<hr>
+    		<h2>◇スキル</h2>
+    		<h3> 言語(バックエンドは太線)</h3>
+    		<ul>
+    		<li>基礎 : <b>C, C++, Java</b>, JavaFX, VB.NET, VBA, HTML5, PHP, Shellなど</li>
+    		<li>応用 : <b>Python(機械学習,Tkinter,OpenCV, WebScrapingなど)</b></li>
+    		<li>学習中 : <b>Golang全般</b>, Java(Spring), Bash Shell, (JavaScript)</li>
+    		</ul>
+    		<h3> その他</h3>
+    		<ul>
+    		<li>基礎 : サーバ構築・運用(実機+CentOS7, VPS), DB(Access, MySQL), RTP・UDP, DTMなど</li>
+    		<li>応用 : PC組み立て(経験10以上), ドラム</li>
+    		<li>学習中 : ネットワークの知識, プロジェクト進行, RDBなど</li>
+    		</ul>
+    		<hr>
+			<h2>リンク集</h2>
+			<ul>
+			<li><a href="https://twitter.com/bluelightcloud">Twitter</a></li>
+			<li><a href="https://www.bluelightcloud.xyz/wordpress">ブログ</a></li>
+			<li><a href="https://github.com/Bluelightcloud">GitHub</a></li>
+			<li>西野尚利概論(工事中)</li>
+			</ul>
+		</div>
+		<center>
+		<p>最終更新日 : 2021/7/26</p>
+		<footer>
+        	<p><font size="2">© 2021 Bluelightcloud</font></p>
+    	</footer>
+    	</center>
+    </body>
+</html>
